1b9e5db987b5bd9efd12a67be1677d4c746bc006
Shadnix <shadnix@arcor.de>
2019-05-25 17:28:06 +0200

    Projektdateien hinzufügen.

---
+++ Level Pack Panel Mover/Logger.cs
@@ -0,0 +1,9 @@
+﻿using IPALogger = IPA.Logging.Logger;
+
+namespace Level_Pack_Panel_Mover
+{
+    internal static class Logger
+    {
+        public static IPALogger log { get; set; }
+    }
+}
---
+++ Level Pack Panel Mover/PluginConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Level_Pack_Panel_Mover
+{
+    internal class PluginConfig
+    {
+        public bool RegenerateConfig = true;
+    }
+}
